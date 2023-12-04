e8b2b801c1a3ad073c63f8ecf1b57cc211d5c9f7
a-dugonin <a_dugonin@mail.ru>
2023-12-04 19:47:22 +0300

    fix after review

--- cohabitation.py
+++ cohabitation.py
@@ -5,21 +5,18 @@ class Human:
         self.home = home
 
     def __str__(self):
-        name = 'Антон'
-        fridge = None
-        result = f"Имя: {name}, fridge: {fridge or 'отсутствует'}"
-        return result
+        return f"Имя: {self.name}, сытость: {self.satiety}, {self.home or 'дом отсутствует'}"
 
     def add_house(self, home):
         self.home = home
 
     def eat(self):
-        """Метод позволяет человеку поесть"""
+        """ Метод позволяет человеку поесть """
         self.satiety += 25
         self.home.fridge.food -= 5
 
     def work(self):
-        """Метод позволяет отправить человека на работу"""
+        """ Метод позволяет отправить человека на работу """
         self.satiety -= 20
         self.home.bedside_table.money += 40
 
@@ -27,12 +24,12 @@ class Human:
         self.satiety -= 10
 
     def go_for_food(self):
-        """Метод отправляет человека за едой"""
+        """ Метод отправляет человека за едой """
         self.home.fridge.food += 30
         self.home.bedside_table.money -= 70
 
     def life(self, num):
-        """Метод позволяет прожить один день"""
+        """ Метод позволяет прожить один день """
         if self.satiety < 20:
             self.eat()
         elif self.home.fridge.food < 10:
@@ -48,38 +45,38 @@ class Human:
 
 
 class House:
-    def __init__(self, fridge: 'Refrigerator' = None | object, bedside_table: 'Nightstand' = None | object):
+    def __init__(self, fridge: 'Refrigerator' = None, bedside_table: 'Nightstand' = None):
         self.fridge = fridge
         self.bedside_table = bedside_table
 
-    def add_refrigerator(self, fridge: 'Refrigerator' = None | object):
+    def add_refrigerator(self, fridge: 'Refrigerator' = None):
         self.fridge = fridge
 
-    def add_nightstand(self, bedside_table: 'Nightstand' = None | object):
+    def add_nightstand(self, bedside_table: 'Nightstand' = None):
         self.bedside_table = bedside_table
 
     def __str__(self):
-        return f'{self.fridge.food}, {self.bedside_table.money}'
+        return f'{self.fridge or 'еды нет'}, {self.bedside_table or 'денег нет'}'
 
 
 class Refrigerator:
     def __init__(self, food: int = 50):
         self.food = food
 
+    def __str__(self):
+        return f'еды в холодильнике: {self.food}'
+
 
 class Nightstand:
     def __init__(self, money: int = 0):
         self.money = money
 
-
-def init_human(name: str):
-    """Инициализация игроков"""
-    human = Human(name)
-    return human
+    def __str__(self):
+        return f'денег в тумбочке: {self.money}'
 
 
 def init_home():
-    """Инициализация дома"""
+    """ Инициализация дома """
     home = House()
     fridge = Refrigerator()
     bedside_table = Nightstand()
@@ -90,10 +87,11 @@ def init_home():
 
 def main():
     import random
-    humans = [init_human(name='Anton'), init_human(name='Anna')]
+    humans = [Human(name='Anton'), Human(name='Anna')]
     home = init_home()
     for human in humans:
         human.add_house(home)
+        print(human)
 
     for day_index in range(1, 366):
         for human in humans:
